13af4ec4484a056e5ba488607114d9668fe14614
Diego Alves <diegoalvesdc12@gmail.com>
2023-11-14 20:09:22 -0300

    Tela de cadastro

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StyleSheet, View } from 'react-native'
 import Head from './src/head'
 import Footer from './src/footer'
-import Body from './src/body';
+import Body from './src/body'
+import Login from './src/telaDeLogin/login'
+import Register from './src/telaDeLogin/register'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Head/>
-      <Body/>
-      <Footer/>   
+      {/* <Head />
+      <Body />
+      <Footer /> */}
+      <Register />
     </View>
   )
 }
--- src/body/style.js
+++ src/body/style.js
@@ -1,139 +1,129 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-     marginTop:35,
-     marginEnd:5,
-    
-    
-    },
-    searchBox:{
-        flexDirection:"row",
-        alignItems:"center",
-      
+  container: {
+    marginTop: 35,
+    marginEnd: 5
+  },
+  searchBox: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  txt: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    backgroundColor: '#33415c',
+    color: 'white',
+    padding: 8,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
+  input: {
+    height: 40,
+    width: 188,
+    margin: 0,
+    borderWidth: 1,
+    borderColor: '#33415c',
+    padding: 8,
+    opacity: 0.6
+  },
 
-    },
-    txt:{
-        fontWeight:"bold",
-        fontSize:18,
-        backgroundColor:"#33415c",
-        color:"white",
-        padding:8,
-        borderTopLeftRadius:10,
-        borderBottomLeftRadius: 10,
+  btn: {
+    backgroundColor: '#0466c8',
+    padding: 10,
+    borderBottomRightRadius: 10,
+    borderTopRightRadius: 10
+  },
+  primary: {
+    padding: 5,
+    backgroundColor: '#4682B4',
+    alignItems: 'center',
+    width: 310,
+    height: 120,
+    borderRadius: 10,
+    marginTop: 10
+  },
+  txt1: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 30,
+    fontSize: 25
+  },
+  cont2: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  second: {
+    padding: 5,
+    backgroundColor: '#4682B4',
+    alignItems: 'center',
+    width: 150,
+    height: 100,
+    borderRadius: 10,
+    marginTop: 15,
+    marginRight: 5
+  },
+  txt2: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 25,
+    fontSize: 25
+  },
+  third: {
+    padding: 5,
+    backgroundColor: '#4682B4',
+    alignItems: 'center',
+    width: 150,
+    height: 100,
+    borderRadius: 10,
+    marginTop: 15
+  },
+  txt3: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 25,
+    fontSize: 25
+  },
 
+  cont3: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  for: {
+    padding: 5,
+    backgroundColor: '#4682B4',
+    alignItems: 'center',
+    width: 150,
+    height: 100,
+    borderRadius: 10,
+    marginTop: 15,
+    marginRight: 5
+  },
+  txt4: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 25,
+    fontSize: 25
+  },
+  five: {
+    padding: 5,
+    backgroundColor: '#4682B4',
+    alignItems: 'center',
+    width: 150,
+    height: 100,
+    borderRadius: 10,
+    marginTop: 15
+  },
+  txt5: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 25,
+    fontSize: 25
+  },
+  img: {
+    width: 50,
+    height: 100
+  }
+})
 
-    },
-    input:{
-        
-        height:40,
-        width:188,
-        margin:0,
-        borderWidth:1,
-        borderColor:"#33415c",
-        padding:8,
-        opacity:0.6,
-        
-    },
-
-
-   btn:{
-    backgroundColor:"#0466c8",
-    padding:10,
-    borderBottomRightRadius:10,
-    borderTopRightRadius:10,
-    
-   },
-   primary:{
-    padding:5,
-    backgroundColor:"#4682B4",
-    alignItems:"center",
-    width:310,
-    height:120,
-    borderRadius:10,
-    marginTop:10,
-   },
-   txt1:{
-    color:"white",
-    fontWeight:"bold",
-    marginTop:30,
-    fontSize:25,
-   },
-   cont2:{
-    flexDirection:"row",
-    alignItems:"center",
-   },
-   second:{
-    padding:5,
-    backgroundColor:"#4682B4",
-    alignItems:"center",
-    width:150,
-    height:100,
-    borderRadius:10,
-    marginTop:15,
-    marginRight:5,
-   },
-   txt2:{
-    color:"white",
-    fontWeight:"bold",
-    marginTop:25,
-    fontSize:25,
-   },
-   third:{
-    padding:5,
-    backgroundColor:"#4682B4",
-    alignItems:"center",
-    width:150,
-    height:100,
-    borderRadius:10,
-    marginTop:15,
-   },
-   txt3:{
-    color:"white",
-    fontWeight:"bold",
-    marginTop:25,
-    fontSize:25,
-   },
-
-   cont3:{
-    flexDirection:"row",
-    alignItems:"center",
-   },
-   for:{
-    padding:5,
-    backgroundColor:"#4682B4",
-    alignItems:"center",
-    width:150,
-    height:100,
-    borderRadius:10,
-    marginTop:15,
-    marginRight:5,
-   },
-   txt4:{
-    color:"white",
-    fontWeight:"bold",
-    marginTop:25,
-    fontSize:25,
-   },
-   five:{
-    padding:5,
-    backgroundColor:"#4682B4",
-    alignItems:"center",
-    width:150,
-    height:100,
-    borderRadius:10,
-    marginTop:15,
-   },
-   txt5:{
-    color:"white",
-    fontWeight:"bold",
-    marginTop:25,
-    fontSize:25,
-   },
-   img:{
-    width:50,
-    height:100,
-   },
-  });
-
-  export default styles;
+export default styles
