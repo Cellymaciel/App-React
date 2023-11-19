1ba0683625ad08484a38e625d0aa15d6dcf65cd6
Cellymaciel <cellymaciel005@gmail.com>
2023-11-19 20:27:38 -0300

    teste

--- src/head/index.js
+++ src/head/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import * as React from 'react';
 import { View, Image, TouchableOpacity, Button } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import styles from './style';
 
 
